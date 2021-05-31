f58870c79fe271b3015f69ab6b6befe909f804ff
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2021-05-31 09:15:44 +0200

    voice off

--- _rstats_installers/rupdater.sh
+++ _rstats_installers/rupdater.sh
@@ -16,31 +16,31 @@ echo "+ * created by:     Sander W. van der Laan | s.w.vanderlaan-2@umcutrecht.n
 echo "+ * last edited on: 2019-03-07                                                     +"
 echo "+                                                                                  +"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-say -v Karen "Running 'R Statistics Updater', version 1.3.3. Today is $(date)."
+#say -v Karen "Running 'R Statistics Updater', version 1.3.3. Today is $(date)."
 echo ""
 echo "* First, updating R..."
-say -v Karen "First, updating R."
+#say -v Karen "First, updating R."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/RStats_Updater_v1.r
 echo ""
 echo "* Now, updating general statistics packages..."
-say -v Samantha "Now, updating general statistics packages."
+#say -v Samantha "Now, updating general statistics packages."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_GENERAL_v1.r
 echo ""
 echo "* Checking for updates to advanced statistics packages..."
-say -v Samantha "Checking for updates to advanced statistics packages."
+#say -v Samantha "Checking for updates to advanced statistics packages."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_ADVANCED_v1.r
 echo ""
 echo "* Moving on to updating genetics packages..."
-say -v Samantha "Moving on to updating genetics packages."
+#say -v Samantha "Moving on to updating genetics packages."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_GENETICS_v1.r
 echo "* Also updating additional specific RNA sequence, Omics, and DNA methylation packages..."
-say -v Samantha "Also updating additional specific RNA sequence, omics, and DNA methylation packages."
+#say -v Samantha "Also updating additional specific RNA sequence, omics, and DNA methylation packages."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_OMICS_v1.r
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_DNAm_v1.r
 echo "* Finally, updating MR-analyses packages..."
-say -v Samantha "Finally, updating MR-analyses packages."
+#say -v Samantha "Finally, updating MR-analyses packages."
 Rscript $HOME/git/swvanderlaan/HerculesToolKit/_rstats_installers/Rstats_MR_v1.r
 echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Hey mate, I'm all done. Closing off. Have a nice day and a 🍺 . The date is $(date)."
-say -v Karen "Hey mate, I'm all done. Closing off. Have a nice day and a beer. The date is $(date)."
+#say -v Karen "Hey mate, I'm all done. Closing off. Have a nice day and a beer. The date is $(date)."
