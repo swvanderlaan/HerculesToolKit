78ea5e16801c1051bb4a0cdb0482521cc7f4f32d
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2022-04-20 16:53:10 +0200

    edits

--- _rstats_installers/install_worcs.R
+++ _rstats_installers/install_worcs.R
@@ -1,12 +1,16 @@
-# Install WORCS
+################################################################################
+###                        SCRIPT to install WORCS                           ###
+################################################################################
 
-if(!requireNamespace("remotes"))install.packages("remotes")
+# https://github.com/cjvanlissa/worcs
+
+if(!requireNamespace("remotes"))
+  install.packages("remotes")
+
 remotes::install_github("cjvanlissa/worcs", dependencies = TRUE, update = "never")
-tinytex::install_tinytex()
-worcs::git_user("swvanderlaan", "s.w.vanderlaan@gmail.com", overwrite = TRUE)
 
-install.packages("RcppArmadillo")
+tinytex::install_tinytex()
+
+worcs::git_user("Sander W. van der Laan", "s.w.vanderlaan@gmail.com", overwrite = TRUE)
 
 remotes::install_github("crsh/papaja", dependencies = TRUE, update = "never")
-
-
